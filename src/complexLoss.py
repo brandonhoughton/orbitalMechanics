e8310ee5b8a9b75963d61fb0b5d48ae786719f2a
Brandon Houghton <bhoughton@cmu.edu>
2018-12-17 13:00:13 -0500

    updated layers to support trippleLayer and other train/test options

--- src/complexLoss.py
+++ src/complexLoss.py
@@ -42,10 +42,10 @@ def singleLayer(X, outDim = 50, name = False):
         head = tf.layers.dense(X, outDim, activation=tf.nn.sigmoid, use_bias=True)
     return head
 
-def trippleLayer(X, outDim = 16):
-    head = tf.layers.dense(X, 64, activation=tf.nn.sigmoid, name = "dense_1", use_bias=True, reuse=reuse_)
-    head = tf.layers.dense(head, 24, activation=tf.nn.sigmoid, name = "dense_2", use_bias=True, reuse=reuse_)
-    head = tf.layers.dense(head, outDim, activation=tf.nn.sigmoid, name = "dense_3", use_bias=True, reuse=reuse_)
+def trippleLayer(X, outDim = 16, name = False):
+    head = tf.layers.dense(X, 64, activation=tf.nn.sigmoid, name = "dense_1", use_bias=True)
+    head = tf.layers.dense(head, 2, activation=tf.nn.sigmoid, name = "dense_2", use_bias=True)
+    head = tf.layers.dense(head, outDim, activation=tf.nn.sigmoid, name = "dense_3", use_bias=True)
     return head
 
 
@@ -58,9 +58,9 @@ pre_train_steps  = 1000
 viz_step         = 999999999999999
 #######################
 a = 0.001 # GradNorm Weight
-b = 0.00000001 # Prediction Weight
+b = 0.000001 # Prediction Weight
 g = 0.001  # Scale for Phi
-lr = 0.01 # Learning Rate
+lr = 0.01  # Learning Rate
 #######################
 
 saveDir = input("Name this run...")
@@ -69,7 +69,9 @@ saveDir = input("Name this run...")
 def main():
 
     # Load data
-    scale, offset, (train_Xp, test_Xp, train_X, test_X, train_F, test_F, train_Y, test_Y), benchmark = get_data_segmented(shuffle=False, seed=42)
+    # scale, offset, (train_Xp, _, train_X, _, train_F, _, train_Y, _), benchmark = get_data_segmented(shuffle=False, seed=42)
+    scale, offset, (train_Xp, _, train_X, _, train_F, _, train_Y, _), benchmark = get_data_(shuffle=False)
+    _, _,          (test_Xp,  _, test_X,  _, test_F,  _, test_Y,  _), _ = get_data_(shuffle=False)
 
     # Load data onto GPU memory - ensure network layers have GPU support
     # with tf.device('/gpu:0'):
@@ -83,16 +85,16 @@ def main():
         # Define placeholders
         Xp = tf.placeholder(dtype= tf.float32, shape=[None, 4], name='Xp')
         X = tf.placeholder(dtype= tf.float32, shape=[None, 4], name='X')
-        F = tf.placeholder(dtype= tf.float32, shape=[None, 4], name = 'F')
+        F = tf.placeholder(dtype= tf.float32, shape=[None, 4], name='F')
         if (b > 0):
             Y = tf.placeholder(dtype= tf.float32, shape=[None, 4], name='Y')
 
         ## Define network
         with tf.name_scope('Base_Network'):
             with tf.variable_scope("auto_weight_sharing") as varScope:
-                baseNetwork = singleLayer(X, outDim=32, name=True)
+                baseNetwork = trippleLayer(X, outDim=32, name=True)
                 varScope.reuse_variables()
-                baseNetPrev = singleLayer(Xp, outDim=32, name=True)
+                baseNetPrev = trippleLayer(Xp, outDim=32, name=True)
 
         with tf.name_scope('Phi'):
             Phi = singleLayer(baseNetwork, outDim = 1)
@@ -147,26 +149,25 @@ def main():
         with tf.name_scope('train'):
             train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
-        # with tf.name_scope('summaries'):
-        #     planet_values = [tf.slice(Phi, [4222 * i, 0], [4222, 1]) for i in range(8)]
-        #     means = [tf.reduce_mean(planetPhi) for planetPhi in planet_values]
-        #     stratified_var = reduce(lambda x,y: (x + y) / 2, [tf.sqrt(tf.reduce_mean(tf.square(var - mean))) for (var, mean) in zip(planet_values, means)])
+        with tf.name_scope('summaries'):
+            planet_values = [tf.slice(Phi, [4222 * i, 0], [4222, 1]) for i in range(8)]
+            means = [tf.reduce_mean(planetPhi) for planetPhi in planet_values]
+            stratified_var = reduce(lambda x,y: (x + y) / 2, [tf.sqrt(tf.reduce_mean(tf.square(var - mean))) for (var, mean) in zip(planet_values, means)])
 
             
 
     # Create summary statistics outside of GPU scope
     variable_summaries(Phi, "PhiSummary")
-    # tf.summary.scalar("PhiByPlanetVar", stratified_var,)
-
-    #variable_summaries(Pred, "Prediction")
+    tf.summary.scalar("PhiByPlanetVar", stratified_var,)
     variable_summaries(gradPhi, "GradPhi")
     variable_summaries(dotProd, "DotProduct")
     variable_summaries(gradMag, "GradMagnitude")
     tf.summary.scalar("GradLoss", gradLoss)
     if(b > 0):
         tf.summary.scalar("PredictiveLoss", predLoss)
+        variable_summaries(Pred, "Prediction")
     tf.summary.scalar("PhiLoss", phiLoss)
-    tf.summary.scalar("Cost", loss)
+    loss_summary = tf.summary.scalar("Cost", loss)
 
     # Collect summary stats for train variables
     merged = tf.summary.merge_all()
@@ -174,24 +175,12 @@ def main():
     #Create list of merged summaries to visualize together on a single graph
     summary_lables = ['earth', 'jupiter', 'mars', 'mercury', 'neptune', 'saturn', 'uranus', 'venus'] 
     summary_lables = [saveDir + '/train/' + planet for planet in summary_lables]
-    # with tf.name_scope('planets'):
-        # summary_list = [tf.summary.merge(variable_summaries_list(planet_val, summary_label)) for (planet_val, summary_label) in zip(planet_values, summary_lables)]
+    with tf.name_scope('planets'):
+        summary_list = [tf.summary.merge(variable_summaries_list(planet_val, summary_label)) for (planet_val, summary_label) in zip(planet_values, summary_lables)]
 
 
     # Create checkpoint saver
     saver = tf.train.Saver()
-
-    # Visualization mesh:
-    nx, ny = (100, 100)
-
-    x = np.linspace(-1, 1, nx)
-    y = np.linspace(-1, 1, ny)
-    Xv, Yv = np.meshgrid(x, y)
-    Xv = np.reshape(Xv, (-1))
-    Yv = np.reshape(Yv, (-1))
-    T = np.arctan2(Yv, Xv) + (math.pi / 2.0)
-    R = np.sqrt((Yv) ** 2 + (Xv) ** 2)
-    U, V = R * np.cos(T), R * np.sin(T)
 
     # Train the model
     with tf.Session() as sess:
@@ -199,23 +188,30 @@ def main():
         
         #timeStr = datetime.datetime.today().strftime('%Y-%m-%d_%H-%M')
         train_writer = tf.summary.FileWriter(J('.',saveDir,'train', 'alpha-' + str(a) + 'beta-' + str(b) + 'gama-' + str(g)), sess.graph)
+        test_writer = tf.summary.FileWriter(J('.',saveDir,'test', 'alpha-' + str(a) + 'beta-' + str(b) + 'gama-' + str(g)), sess.graph)
         writers = [tf.summary.FileWriter(name) for name in summary_lables]
-        dic = {'Xp:0':train_Xp, 'X:0':train_X, 'F:0':train_F, 'Y:0':train_Y}
+        dic = {'Xp:0':train_Xp, 'X:0':train_X, 'F:0':train_F}
+        test_dic = {'Xp:0':test_Xp, 'X:0':test_X, 'F:0':test_F}
+        if b > 0:
+            dic['Y:0'] = train_Y
+            test_dic['Y:0'] = test_Y
 
         for epoch in range(train_epoch+1):
             
             if epoch > pre_train_steps:
                 if epoch % summary_step == 0:
-                    summary = sess.run([merged],feed_dict=dic)[0]
-                    train_writer.add_summary(summary, epoch)
-                    # [writer.add_summary(sess.run([summary],feed_dict=dic)[0], epoch) for (writer,summary) in zip(writers,summary_list)]
+                    summary = sess.run([merged], feed_dict=test_dic)[0]
+                    test_writer.add_summary(summary, epoch)
+                    loss_ = sess.run([loss_summary], feed_dict=dic)[0]
+                    train_writer.add_summary(loss_, epoch)
+                    [writer.add_summary(sess.run([summary],feed_dict=test_dic)[0], epoch) for (writer,summary) in zip(writers, summary_list)]
 
                 if epoch % display_step == 0:
                     loss_ = sess.run(loss,feed_dict=dic)
                     print(loss_, epoch)
 
                 if epoch % checkpoint_int == 0:
-                    saver.save(sess,save_path=J('.',saveDir,'network',str(epoch)))
+                    saver.save(sess, save_path=J('.', saveDir, 'network', str(epoch)))
 
                 # if epoch % viz_step == 0:
                 #     phi = sess.run([Phi],feed_dict=viz_dic)
